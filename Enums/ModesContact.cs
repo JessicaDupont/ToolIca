08e3560011406b429cfc40c7fa5a6fe78ddfe84f
Jessica Dupont <j.dupont@stag.technofuturtic.education>
2021-08-11 11:02:32 +0200

    ajout enums

---
+++ Enums/ModesContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolIca.Enums
+{
+    public enum ModesContact
+    {
+        Email,
+        TelephoneFixe,
+        TelephoneMobile,
+        Fax,
+        Twitter,
+        Instagram,
+        FacebookProfil,
+        FacebookPage,
+        FacebookMessenger,
+        LinkedinProfil,
+        SiteWeb
+    }
+}
---
+++ Geographie/Continents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolIca.Geographie
+{
+    public enum Continents
+	{
+        Afrique, 
+		AmériqueNord, 
+		AmériqueSud, 
+		Antarctique, 
+		Asie, 
+		Europe,
+		Océanie, 
+		Zealandia
+    }
+}
