563b5c411d8184e92195b7b12f7fdc1b32cbf749
Jessica Dupont <85496160+JessicaDupont@users.noreply.github.com>
2022-10-06 13:29:30 +0200

    Create IEntityActiveBase.cs

---
+++ DataBases/EntityFramework/Bases/IEntityActiveBase.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolIca.DataBases.EntityFramework.Bases
+{
+    public interface IEntityActiveBase
+    {
+        public bool Active { get; set; }
+        [Column("date_active", TypeName = "DateTime2")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastActive { get; set; }
+    }
+}
