2d312370ca74860e570c32ca31ac308c5c4600b9
Jessica Dupont <85496160+JessicaDupont@users.noreply.github.com>
2022-10-06 13:31:35 +0200

    Create IEntityAlertBase.cs

---
+++ DataBases/EntityFramework/Bases/IEntityAlertBase.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolIca.DataBases.EntityFramework.Bases
+{
+    public interface IEntityAlertBase
+    {
+        public bool Alert { get; set; }
+        [Column("date_alert", TypeName = "DateTime2")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastAlert { get; set; }
+        public int NbAlert { get; set; }
+    }
+}
